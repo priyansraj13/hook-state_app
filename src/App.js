9b4548ca093467ce320a1862a656bfb47cd5c780
priyansh <priyanshraj2021@gmail.com>
2023-10-22 23:29:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+
+import Colorizer from './Colorize';
+import Counter from './Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Colorizer />
+
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
